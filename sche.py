935aaadd3ba3d724b0173a192f3b3b5761a0e79a
quangto188 <144829938+quangto188@users.noreply.github.com>
2024-05-31 23:23:05 +0700

    update

--- sche.py
+++ sche.py
@@ -1,29 +1,33 @@
 import threading
 import _thread
+
 SCH_MAX_TASKS = 10  # Define this as per your requirement
 SCH_tasks_G = [None] * SCH_MAX_TASKS
 current_index_task = 0
 Error_code_G = 0
 
 class Task:
-    def __init__(self, pTask=None, Delay=0, RunMe=0):
+    def __init__(self, pTask=None, Delay=0, RunMe=0, *args, **kwargs):
         self.pTask = pTask
         self.Delay = Delay
         self.RunMe = RunMe
+        self.args = args
+        self.kwargs = kwargs
+
 
 def SCH_Init():
     for i in range(SCH_MAX_TASKS):
         SCH_Delete_Task(i)
 
-def SCH_Add_Task(pFunction, DELAY):
+def SCH_Add_Task(pFunction, DELAY, *args, **kwargs):
     global current_index_task
     if current_index_task < SCH_MAX_TASKS:
-        SCH_tasks_G[current_index_task] = Task(pFunction, DELAY,0)
+        SCH_tasks_G[current_index_task] = Task(pFunction, DELAY, 0, *args, **kwargs)
         current_index_task = current_index_task + 1
         return 0  # RETURN_NORMAL
     else:
         #Error_code_G = 1  # ERROR_SCH_TOO_MANY_TASKS
-        return 1  # RETURN_ERRO
+        return 1  # RETURN_ERROR
     
 def SCH_Update():
     global current_index_task
@@ -39,15 +43,10 @@ def SCH_Dispatch_Tasks():
         i=0
         while (i < current_index_task ):
             if SCH_tasks_G[i].RunMe>0:
-                # print("RUN TASK:" ,i)
-                #_thread.start_new_thread(target =SCH_tasks_G[i].pTask())
-                SCH_tasks_G[i].pTask() # Run function
+                _thread.start_new_thread(SCH_tasks_G[i].pTask, SCH_tasks_G[i].args, SCH_tasks_G[i].kwargs)
                 SCH_Delete_Task(i)
-                # print("DEL SUCCESS TASK", i)
-                # print(" CON LAI ",current_index_task, "TASK")
-                # print(SCH_tasks_G)
                 i-=1
-            i+=1
+            i+=1    
     else:
         return
             
