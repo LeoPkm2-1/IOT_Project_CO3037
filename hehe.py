2f6de101ca0cab18a93687cb50740a0c637efd63
leo <thinhphatmai2001@gmail.com>
2024-06-07 22:02:55 +0700

    '.'

--- hehe.py
+++ hehe.py
@@ -1,3 +1,27 @@
+relay1_ON  = [1, 6, 0, 0, 0, 255, 201, 138]
+relay1_OFF = [1, 6, 0, 0, 0, 0, 137, 202]
+
+relay2_ON  = [2, 6, 0, 0, 0, 255, 201, 185]
+relay2_OFF = [2, 6, 0, 0, 0, 0, 137, 249]
+
+relay3_ON  = [3, 6, 0, 0, 0, 255, 200, 104]
+relay3_OFF = [3, 6, 0, 0, 0, 0, 136, 40]
+
+relay4_ON  = [4, 6, 0, 0, 0, 255, 201, 223]
+relay4_OFF = [4, 6, 0, 0, 0, 0, 137, 159]
+
+relay5_ON  = [5, 6, 0, 0, 0, 255, 200, 14]
+relay5_OFF = [5, 6, 0, 0, 0, 0, 136, 78]
+
+relay6_ON  = [6, 6, 0, 0, 0, 255, 200, 61]
+relay6_OFF = [6, 6, 0, 0, 0, 0, 136, 125]
+
+relay7_ON  = [7, 6, 0, 0, 0, 255, 201, 236]
+relay7_OFF = [7, 6, 0, 0, 0, 0, 137, 172]
+
+relay8_ON  = [8, 6, 0, 0, 0, 255, 201, 19]
+relay8_OFF = [8, 6, 0, 0, 0, 0, 137, 83]
+
 print("Sensors and Actuators")
 
 import time
@@ -34,14 +58,14 @@ except:
 # relay1_ON  = [1, 6, 0, 0, 0, 255, 201, 138]
 # relay1_OFF = [1, 6, 0, 0, 0, 0, 137, 202]
 
-relay5_ON  = [5, 6, 0, 0, 0, 255, 200, 14]
-relay5_OFF = [5, 6, 0, 0, 0, 0, 136, 78]
+# relay5_ON  = [5, 6, 0, 0, 0, 255, 200, 14]
+# relay5_OFF = [5, 6, 0, 0, 0, 0, 136, 78]
 
 def setDevice1(state):
     if state == True:
-        ser.write(relay5_ON)
+        ser.write(relay2_ON)
     else:
-        ser.write(relay5_OFF)
+        ser.write(relay2_OFF)
     time.sleep(1)
     print(serial_read_data(ser))
 
@@ -63,29 +87,29 @@ def serial_read_data(ser):
     return 0
 
 
-# while True:
-#     setDevice1(True)
-#     time.sleep(1)
-#     setDevice1(False)
-#     time.sleep(1)
-
-soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
-def readTemperature():
-    serial_read_data(ser)
-    ser.write(soil_temperature)
-    time.sleep(1)
-    return serial_read_data(ser)
-
-soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
-def readMoisture():
-    serial_read_data(ser)
-    ser.write(soil_moisture)
-    time.sleep(1)
-    return serial_read_data(ser)
-
 while True:
-    print("TEST SENSOR")
-    print(readMoisture())
+    setDevice1(True)
     time.sleep(1)
-    print(readTemperature())
+    setDevice1(False)
     time.sleep(1)
+
+# soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
+# def readTemperature():
+#     serial_read_data(ser)
+#     ser.write(soil_temperature)
+#     time.sleep(1)
+#     return serial_read_data(ser)
+
+# soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
+# def readMoisture():
+#     serial_read_data(ser)
+#     ser.write(soil_moisture)
+#     time.sleep(1)
+#     return serial_read_data(ser)
+
+# while True:
+#     print("TEST SENSOR")
+#     print(readMoisture())
+#     time.sleep(1)
+#     print(readTemperature())
+#     time.sleep(1)
