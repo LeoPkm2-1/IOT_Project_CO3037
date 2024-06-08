b809128c2eecdd35467db785996a44e4243847dc
leo <thinhphatmai2001@gmail.com>
2024-06-08 20:28:19 +0700

    '.'

--- ref/modbus485_edit.py
+++ ref/modbus485_edit.py
@@ -35,10 +35,12 @@ class Modbus485:
     def modbus485_read_adc(self):
         ser = self.rs485
         bytesToRead = ser.inWaiting()
+        print('_bytesToRead: ',bytesToRead)
         if bytesToRead > 0:
             out = ser.read(bytesToRead)
             data_array = [b for b in out]
-            print(data_array)
+            print("__Buffer: ",out)
+            print('___data_array: ',data_array)
             if len(data_array) >= 7:
                 array_size = len(data_array)
                 value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
