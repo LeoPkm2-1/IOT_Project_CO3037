a3fcd4a757e839bbf41a111d38aea4478fee62e3
leo <thinhphatmai2001@gmail.com>
2024-06-08 13:42:15 +0700

    '.'

--- controllers/rs485_handler.py
+++ controllers/rs485_handler.py
@@ -48,32 +48,10 @@ print(f"portName: {portName}")
 
 try:
     ser = serial.Serial(port=portName, baudrate=115200)
-    print("Open successfully")
+    print("__Open successfully")
 except:
-    print("Can not open the port")
+    print("__Can not open the port")
     
-    
-# def serial_read_data(ser):
-#     bytesToRead = ser.inWaiting()
-#     if bytesToRead > 0:
-#         out = ser.read(bytesToRead)
-#         data_array = [b for b in out]
-#         print(data_array)
-#         if len(data_array) >= 7:
-#             array_size = len(data_array)
-#             value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-#             return value
-#         else:
-#             return -1
-#     return 0
-    
-def set_device_1(state):
-    if state == True:
-        ser.write(relay1_ON)
-    else:
-        ser.write(relay1_OFF)
-    time.sleep(1)
-    print(serial_read_data(ser))
     
 def serial_read_data(ser):
     bytesToRead = ser.inWaiting()
@@ -88,10 +66,90 @@ def serial_read_data(ser):
         else:
             return -1
     return 0
+    
+def setDevice1(state):
+    if state == True:
+        ser.write(relay1_ON)
+    else:
+        ser.write(relay1_OFF)
+    time.sleep(1)
+    print(serial_read_data(ser))
+    
+
+def set_device_1(state):
+    if state == True:
+        ser.write(relay1_ON)
+    else:
+        ser.write(relay1_OFF)
+
+def set_device_2(state):
+    if state == True:
+        ser.write(relay2_ON)
+    else:
+        ser.write(relay2_OFF)
+
+def set_device_3(state):
+    if state == True:
+        ser.write(relay3_ON)
+    else:
+        ser.write(relay3_OFF)
+        
+        
+def set_device_4(state):
+    if state == True:
+        ser.write(relay4_ON)
+    else:
+        ser.write(relay4_OFF)
+
+def set_device_5(state):
+    if state == True:
+        ser.write(relay5_ON)
+    else:
+        ser.write(relay5_OFF)
+        
+def set_device_6(state):
+    if state == True:
+        ser.write(relay6_ON)
+    else:
+        ser.write(relay6_OFF)
+        
+def set_device_7(state):
+    if state == True:
+        ser.write(relay7_ON)
+    else:
+        ser.write(relay7_OFF)
+        
+def set_device_8(state):
+    if state == True:
+        ser.write(relay8_ON)
+    else:
+        ser.write(relay8_OFF)
 
 
-while True:
-    set_device_1(True)
-    time.sleep(2)
-    set_device_1(False)
-    time.sleep(2)
+def set_device_state(deviceId=1,state=True):
+    if deviceId == 1:
+        set_device_1(state=state)
+    elif deviceId == 2:
+        set_device_2(state=state)
+    elif deviceId == 3:
+        set_device_3(state=state)
+    elif deviceId == 4:
+        set_device_4(state=state)
+    elif deviceId == 5:
+        set_device_5(state=state)        
+    elif deviceId == 6:
+        set_device_6(state=state)  
+    elif deviceId == 7:
+        set_device_7(state=state)
+    elif deviceId == 8:
+        set_device_8(state=state)
+
+
+
+
+if __name__ =='__main__':
+    while True:
+        set_device_1(True)
+        time.sleep(2)
+        set_device_1(False)
+        time.sleep(2)
