be0b9a04a3e5fd8254cd56cbb4363aec3679c17c
leo <thinhphatmai2001@gmail.com>
2024-06-10 02:13:28 +0700

    '.'

--- controllers/setting.py
+++ controllers/setting.py
@@ -84,7 +84,7 @@ class Schedule:
         self.scheduleName = scheduleName
         self.cycle = int(cycle) if cycle.strip() else ''
         self.area = int(area) if Utilization.is_valid_int(area) else 0
-        print('____timee_',scheduleStartTime.upper())
+        print('____timee_',scheduleStartTime)
         self.scheduleStartTime = datetime.datetime.strptime(
             scheduleStartTime, self.DATE_TIME_FORMAT) if scheduleStartTime.upper() !='NOW' else datetime.datetime.now()
         # .strptime(self.DATE_TIME_FORMAT)
