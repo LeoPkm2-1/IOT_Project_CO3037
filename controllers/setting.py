b3190c61bcf9e8da10e04fa9f8d84b7343ec8eec
leo <thinhphatmai2001@gmail.com>
2024-06-10 02:16:11 +0700

    '.'

--- controllers/setting.py
+++ controllers/setting.py
@@ -86,7 +86,8 @@ class Schedule:
         self.area = int(area) if Utilization.is_valid_int(area) else 0
         print('____timee_',scheduleStartTime)
         self.scheduleStartTime = datetime.datetime.strptime(
-            scheduleStartTime, self.DATE_TIME_FORMAT) if scheduleStartTime.upper() !='NOW' else datetime.datetime.now()
+            scheduleStartTime, self.DATE_TIME_FORMAT) if scheduleStartTime.upper() !='NOW' else datetime.datetime.now() 
+        print(self.scheduleStartTime)
         # .strptime(self.DATE_TIME_FORMAT)
         self.scheduleEndTime = datetime.datetime.strptime(
             scheduleEndTime, self.DATE_TIME_FORMAT
