f687242aed7bcc6b32dd34d5aa612766084ed91a
khiem.nguyen <nguyencongkhiem510@gmail.com>
2024-06-12 21:04:00 +0700

    Unhide consts.dart file in Mobile App without API KEYS, need to modify this file with your own KEYS before build the App

---
+++ smart_farm/lib/consts.dart
@@ -0,0 +1,6 @@
+const OPENWEATHER_API_KEY = 'api_key';
+
+const ADAFRUIT_IO_USERNAME = 'username';
+const ADAFRUIT_IO_PASSWORD = 'adafruit_io_key';
+const LISTEN_IOT_GATE = 'username/feeds/feed-name';
+const RESPONSE_IOT_GATE = 'username/feeds/feed-name';
